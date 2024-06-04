1c736c31052a2c58c91080d683637337e2970478
Yatemaru <138629727+LeonardoBrito-XLBR@users.noreply.github.com>
2024-06-04 16:46:24 -0300

    Update main.py

--- Netflix/main.py
+++ Netflix/main.py
@@ -1,20 +1,22 @@
+
 #LIMPANDO O TERMINAL DO VS CODE
 import os
+import time
 os.system ("cls || clear")
 
 
 class Usuario(): #não precisava do ()
 
-    def __init__(self, nome, email, plano):
+    def __init__(self, nomeUsu, emailUsu, planoUsu):
 
         #LEMBRE-SE, NÃO FICAR PRESO AO MESMO NOME 
-        self.nome = nome
-        self.email = email
-        self.lista_plano = ['basic', 'premium'] #LISTA DE PLANOS DISPONIVEIS
+        self.nome = nomeUsu
+        self.email = emailUsu
+        self.lista_plano = ['basic', 'premium', 'platina'] #LISTA DE PLANOS DISPONIVEIS
 
         #GARANTINDO MAIS UMA VEZ SE O PLANO ESTA DISPONIVEL
-        if plano in self.lista_plano:
-            self.plano = plano
+        if planoUsu in self.lista_plano:
+            self.plano = planoUsu
         else:
             raise Exception ("Plano Invalido")
         
@@ -25,16 +27,46 @@ class Usuario(): #não precisava do ()
         else:
             print("plano invalido")
 
-    # def ver_filmes (self, filme):
+    def ver_filmes (self):
+        if cliente.plano == "basic":
+
+            print ("01 - LAGOA DOS PATOS")
+        
+        elif cliente.plano == "premium":
+            print("02 - RAMBO")
+
+        elif cliente.plano == "platina":
+            print("O filme disponivel sua categoria - 03 - BAD BOYS 4")
+
+        
+        confirmação = input ("\nComeçar a assistir? ")
+
+
 
 #COLOCANDO VALORES + ARMAZENANDO A CLASS EM UMA NOVA VARIAVEL 
-cliente = Usuario ("Leonardo", "leopato@gmail.com", "basic")
+cliente = Usuario (
+    nomeUsu = input ("Digte o seu nome: "),
+    emailUsu = input ("Digite o seu email: "),
+    planoUsu = input ("Digite qual o seu plano (basic, premium, platina): "),
+)
+
+os.system("cls || clear")
 
 #PLANO ANTERIOMENTE
-print(f"O plano do cliente era: {cliente.plano}")
+print(f"O plano do cliente atual é: {cliente.plano}")
 
 #ENVIANDO UM NOVO PLANO
-cliente.mudarPlano("balalal")
+opcaoPlano = input ("\nDeseja mudar de plano: ")
 
-#ALTUALIZANDO O PLANO
-print(f"O novo plano do cliente {cliente.plano}")
+if opcaoPlano.lower()  == "s":
+    novoPlano = input ("Qual o plano que vc deseja? (basic, premium, platina)? ")
+    cliente.mudarPlano(novoPlano)
+
+elif opcaoPlano.lower() != "s":
+    print(f"\nO seu plano ainda continuar como: {cliente.plano}")    
+
+
+time.sleep(5)
+os.system("cls || clear")
+
+cliente.ver_filmes()
