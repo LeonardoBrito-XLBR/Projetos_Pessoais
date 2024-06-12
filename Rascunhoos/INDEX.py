00f6f8314112556937a2cebf2021009d31a268d8
LeonardoBrito-XLBR <leonardobrito2707@gmail.com>
2024-06-11 23:07:13 -0300

    NOVA forma

--- Rascunhoos/INDEX.py
+++ Rascunhoos/INDEX.py
@@ -1,13 +1,4 @@
-def verificar_item_no_vetor(vetor, item):
-    return item in vetor
+'''
+ESPAÇO PARA PAI
 
-# Exemplo de uso da função
-vetor = [1, 2, 3, 4, 5]
-
-
-item = int (input(" escolha 1 - 5:   ")) 
-
-if verificar_item_no_vetor(vetor, item):
-    print(f"O item {item} está presente no vetor.")
-else:
-    print(f"O item {item} não está presente no vetor.")
+'''
