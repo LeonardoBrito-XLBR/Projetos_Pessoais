b1ae6348b997f72950cb976c29acca12abecc497
LeonardoBrito-XLBR <leonardobrito2707@gmail.com>
2024-07-19 21:59:31 -0300

    dark

---
+++ Rascunhoos/Algoritmo.py
@@ -0,0 +1,37 @@
+import random
+
+num = random.randint(0, 10)
+
+print(f"O número sorteado foi: {num}")
+
+player = {
+"nome": "Chaves",
+"ataque": 2,
+"vida": 10,
+}
+
+monstro = {
+"nome": "Ghatry",
+"ataque": 5,
+"vida": 10,
+}
+
+
+while True:
+    num = random.randint(0, 10)
+    print(f"O número sorteado foi: {num}")
+
+    if num % 2 == 0:
+        player['vida'] -= 1
+        print(f"A vida do jogador agora é: {player['vida']}")
+    else:
+        print("O numero é ímpar")
+
+    continuar = input("\nDeseja continuara batalha? ")
+    print ()
+    if continuar == 'nao':
+        break
+
+#CONTINUAR DEPOIS DAQUI E ANTES DAQUI
+
+print (f"O nome: {player['nome'],  player['vida'], player ['ataque']}")
