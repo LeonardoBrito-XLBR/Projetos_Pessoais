6397ab16967f0bd8a49fd76abf90a86cd5e26eb6
LeonardoBrito-XLBR <leonardobrito2707@gmail.com>
2024-09-24 22:56:32 -0300

    pulo

--- Mario Bros/js/scritp.js
+++ Mario Bros/js/scritp.js
@@ -1,12 +1,28 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector ('.pipe');
 
 const jump = () => {
     mario.classList.add('jump');
-    setTimeout(() => {
-        
+
+    setTimeout(() => {  
     mario.classList.remove('jump');
         
     }, 500);
 }
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition =  window.getComputedStyle(mario).bottom;
+    console.log(marioPosition);
+    
+    
+    if (pipePosition <= 120) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+    }
+
+}, 10);
 
 document.addEventListener('keydown', jump);
