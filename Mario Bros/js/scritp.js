786ff6dff34facafab87fd4330ff49d1262c4966
LeonardoBrito-XLBR <leonardobrito2707@gmail.com>
2024-10-08 22:15:03 -0300

    finalizado !

--- Mario Bros/js/scritp.js
+++ Mario Bros/js/scritp.js
@@ -13,14 +13,23 @@ const jump = () => {
 const loop = setInterval(() => {
 
     const pipePosition = pipe.offsetLeft;
-    const marioPosition =  window.getComputedStyle(mario).bottom;
+    const marioPosition =  +window.getComputedStyle(mario).bottom.replace('px','');
     console.log(marioPosition);
     
     
-    if (pipePosition <= 120) {
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = 'image/game-over.png'
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        clearInterval (loop);
     }
 
 }, 10);
