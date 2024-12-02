cb0fa62b88e808a48c0b410e647c89c83613b9ba
LeonardoBrito-XLBR <leonardobrito2707@gmail.com>
2024-12-01 22:39:58 -0300

    tags de rolagem

--- Catalogo de Filmes/js/script.js
+++ Catalogo de Filmes/js/script.js
@@ -1,18 +1,26 @@
+const formPesquisa = document.querySelector("form");
+const pages = document.querySelector(".pages")
+const totalPages = document.querySelector(".total-pages")
+const atualPages = document.querySelector(".current-page")
+const btnSearch = document.querySelector("#botao")
+
 const apikey = '346c304d';
-const frmPesquisa = document.querySelector("form");
 
+let page = 1;
 
-frmPesquisa.onsubmit = (ev) => {
-    ev.preventDefault();
+const searchMovies = (page) => {
+    console.log(page)
     
-    const pesquisa = ev.target.pesquisa.value;
+    const pesquisa = document.getElementById("pesquisa").value
+    // const pesquisa = ev.target.pesquisa.value;
+
     
     if (pesquisa === ""){
         alert("Preencha o campo!")
         return;
     }
 
-    fetch(`https://www.omdbapi.com/?s=${pesquisa}&apikey=${apikey}`)
+    fetch(`https://www.omdbapi.com/?s=${pesquisa}&apikey=${apikey}&page=${page}`)
         .then(result => result.json())
         .then(json => carregaLista(json));
 }
@@ -21,6 +29,11 @@ frmPesquisa.onsubmit = (ev) => {
 const carregaLista = (json) => {
     const lista = document.querySelector(".lista")
     lista.innerHTML = "";
+
+    const totalResults = Number(json.totalResults)
+    totalPages.textContent = `${Math.round(totalResults /10)}`
+
+
 
     if (json.Response == 'False'){
         alert('Nenhum filme encontrado');
@@ -37,4 +50,43 @@ const carregaLista = (json) => {
 
         lista.appendChild(item);
     });
+
+    pages.classList.add("active") //MESMA LOGICA DO DARK E LIGHT
 }
+
+const changesPages = (e) => {
+    const buttonClicked = e.srcElement;
+    console.log(buttonClicked)
+    
+
+    if (buttonClicked.classList.contains('last-page')){
+        if (page<=1) return;
+
+        page -=1;
+        atualPages.textContent = page
+        searchMovies(page)
+        console.log("Esquerda")
+    }
+    
+    if (buttonClicked.classList.contains('next-page')){
+        if(page>=totalPages.textContent) return;
+        
+        page +=1;
+        atualPages.textContent = page
+        searchMovies(page)
+        console.log("Direita")
+    }
+}
+
+
+pages.addEventListener("click", changesPages);
+btnSearch.addEventListener("click", searchMovies);
+
+formPesquisa.addEventListener('submit', (e) => e.preventDefault()) 
+
+document.getElementById("pesquisa").addEventListener('change', () =>{
+    page=1
+    atualPages.textContent = page;
+} )
+
+
